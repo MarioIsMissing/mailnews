9a62b99aefaeb9c32231372ecd15d045a0dbff2d
Matt A. Tobin <email@mattatobin.com>
2018-09-28 22:35:01 -0400

    Move navigator to subdirectory for git repo mangling reasons

--- app/defaults/pref/application-l10n.js
+++
@@ -1,6 +0,0 @@
-//@line 4 "d:\Development\moonchild-central\uxp\application\navigator\locales\en-US\suite-l10n.js"
-
-//@line 6 "d:\Development\moonchild-central\uxp\application\navigator\locales\en-US\suite-l10n.js"
-
-pref("general.useragent.locale", "en-US");
-pref("spellchecker.dictionary", "en-US");
